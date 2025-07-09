26177a58239bc4fb7104ee78c47869b8677914d7
Deepak <deepakdharsh01@gmail.com>
2025-07-09 15:20:44 +0530

    added somechanges

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
 
   return (
     <div>
-      First name Development another: 
+      name: 
       {user?.username}
       password: 
       {user?.password}
